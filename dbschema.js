703d3db702dc21f88100d26b4d7af23ead813e4d
kpose <ganihujude@gmail.com>
2019-12-22 05:06:19 +0100

    first commmit

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    posts: [
+        {
+            userHandle: 'user',
+            body: 'this is a post body',
+            createdAt: '2019-05-14T11:46:01:0182',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+};
